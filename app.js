fec21a1d49aa6776b39db43ac2c92228f014edc1
Diego Sandoval <diegosv6910@gmail.com>
2021-08-03 19:09:18 -0500

    Comentando Codigo para explicar

--- app.js
+++ app.js
@@ -1,26 +1,42 @@
+//Configuracion de express.
 const express = require('express');
 const router = express.Router();
-path = require('path');
+const app = express();
 
-const bodyParser = require('body-parser');
+//Configuracion Middlewares
+path = require('path');
 const fs = require('fs');
 const fileupload = require('express-fileupload');
 const FileController = require('./controllers/FileController');
+const fileController = new FileController();
+
+//Confiiguracion Body-Parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileupload());
 
 const{
     readExcel
 } = require('./controllers/ExcelController')
 
-const app = express();
-const fileController = new FileController();
+//Comportamientos de rutas
+//Ruta Principal
+app.get('/', function (req, res) {
+    const fs = require('fs');
+    var files = fs.readdirSync('./uploads')
+    res.render('index', { arrayData: files });
+});
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(fileupload());
-
+//Ruta post /subir-archivo.
 router.post('/subir-archivo', fileController.subirArchivo);
-// router.post('/generar-reporte', ExcelController.leerArchivo)
 
+//Ruta get /reports/:docName
+app.get('/reports/:docName', function (req, res){
+    readExcel(req.params.docName, res);
+})
+
+//Uso de router para los errores y excepciones.
 router.use(function (req, res) {
     res.status(404).json({
         error: true,
@@ -28,27 +44,21 @@ router.use(function (req, res) {
     });
 });
 
+//Uso de una ruta como predeterminada.
 app.use('/api', router);
 
+//Configuracion de los directorios estaticos.
 app.use(express.static(__dirname));
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/lib'));
 app.set('views', path.join(__dirname, '/public/views'));
 app.set('view engine', 'ejs');
 
-app.get('/', function (req, res) {
-    const fs = require('fs');
-    var files = fs.readdirSync('./uploads')
-    res.render('index', { arrayData: files });
-});
-
-app.get('/reports/:docName', function (req, res){
-    readExcel(req.params.docName, res);
-})
-
+//Inicializamos el servidor.
 var port = 3000;
 app.listen(port, function () {
     console.log('Server', process.pid, 'listening on port', port);
 });
 
+//Exportamos app.
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
-// importamos las dependencias necesarias
-var router = require('express').Router();
-
-// definimos el comportamiento en la raíz del endpoint
+// Comportamiento raiz.
 router.get('/', (req, res)=>{
   res.send('Hello');
 });
+
 
 // exportamos nuestro nuevo router
 module.exports = router;
